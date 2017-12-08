9d1735a2de4b1838fce45b3eeac6ee115a13810c
Mikko Multanen <mikko.multanen@gmail.com>
2017-12-09 01:02:04 +0200

    Random cave using 2D Destruction lib

---
+++ Assets/Scripts/CaveGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveGenerator : ExplodableAddon
+{
+	void Start() {
+		GetComponent<Explodable> ().explode ();
+	}
+
+	public override void OnFragmentsGenerated(List<GameObject> fragments) {
+		foreach (GameObject fragment in fragments) {
+			if (Random.Range (0, 100) > 50) {
+				if (Application.isEditor)
+				{
+					DestroyImmediate(fragment);
+				}
+				else
+				{
+					Destroy(fragment);
+				}
+			} else {
+				Rigidbody2D fragRb = fragment.GetComponent<Rigidbody2D> ();
+				fragRb.bodyType = RigidbodyType2D.Static;
+				fragment.AddComponent<EarthBlock>();
+			}
+		}
+	}
+}
+
