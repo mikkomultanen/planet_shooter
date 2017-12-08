132e4bae12c209b12b3df973d176cc921be8b297
Mikko Multanen <mikko.multanen@gmail.com>
2017-12-09 01:29:38 +0200

    Fix fragment generation when stopping play mode

--- Assets/Scripts/CaveGenerator.cs
+++ Assets/Scripts/CaveGenerator.cs
@@ -1,28 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+[RequireComponent (typeof(Explodable))]
 public class CaveGenerator : ExplodableAddon
 {
-	void Start() {
-		GetComponent<Explodable> ().explode ();
+	void Awake ()
+	{
+		if (!Application.isEditor || EditorApplication.isPlaying) {
+			GetComponent<Explodable> ().explode ();
+		}
 	}
 
-	public override void OnFragmentsGenerated(List<GameObject> fragments) {
+	public override void OnFragmentsGenerated (List<GameObject> fragments)
+	{
 		foreach (GameObject fragment in fragments) {
 			if (Random.Range (0, 100) > 50) {
-				if (Application.isEditor)
-				{
-					DestroyImmediate(fragment);
-				}
-				else
-				{
-					Destroy(fragment);
+				if (Application.isEditor) {
+					DestroyImmediate (fragment);
+				} else {
+					Destroy (fragment);
 				}
 			} else {
 				Rigidbody2D fragRb = fragment.GetComponent<Rigidbody2D> ();
 				fragRb.bodyType = RigidbodyType2D.Static;
-				fragment.AddComponent<EarthBlock>();
+				fragment.AddComponent<EarthBlock> ();
 			}
 		}
 	}
