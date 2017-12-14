f5a3b5e2327039f1ec128a71340d4b1a9a7217a4
Jari Avikainen <jari.avikainen@gmail.com>
2017-12-14 23:50:59 +0200

    Add damage caused by bullets and scenery

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float health = 100;
+    private float bulletDamage = 10;
+    public ParticleSystem smoke;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bullet") {
+            this.health -= this.bulletDamage;
+        }
+            
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.health -= collision.relativeVelocity.sqrMagnitude / 100;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        bool lowHealth = (this.health <= 20);
+		if (lowHealth && smoke.isStopped)
+        {
+            smoke.Play();
+        }
+        if (lowHealth != smoke.isPlaying)
+        {
+            if (lowHealth) smoke.Play();
+            else smoke.Stop();
+        }
+    }
+}
