65dce2176af0f7fa83a9e86c3a47525e718af0cb
Mikko Multanen <mikko.multanen@gmail.com>
2017-12-17 14:39:32 +0200

    2 player mode

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,51 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerNumber
+{
+	P1,
+	P2,
+	P3,
+	P4
+}
+
 public class PlayerController : MonoBehaviour
 {
-
+	public PlayerNumber playerNumber;
 	public GameObject projectile;
 	public Transform gunPoint;
-	public float maxThrustPower = 20f;
+	public float maxThrustPower = 2000f;
+	public float maxSpeed = 10f;
 	public float fireRate = 0.2f;
 	public ParticleSystem thruster;
 
 	private Rigidbody2D rb;
+	private float originalDrag;
 	private float nextFire = 0.0f;
 	private float gravityForceMagnitude;
 	private bool isInWater = false;
+	private string xAxis;
+	private string yAxis;
+	private string fire1Button;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rb = gameObject.GetComponent<Rigidbody2D> ();
+		originalDrag = rb.drag;
 		gravityForceMagnitude = rb.gravityScale * rb.mass * (-9.81f);
+		xAxis = playerNumber.ToString () + " Horizontal";
+		yAxis = playerNumber.ToString () + " Vertical";
+		fire1Button = playerNumber.ToString () + " Fire1";
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
+	void OnTriggerEnter2D (Collider2D other)
+	{
 		if (other.tag == "Water") {
 			isInWater = true;
 			rb.drag = 5;
 		}
 	}
 
-	void OnTriggerExit2D(Collider2D other) {
+	void OnTriggerExit2D (Collider2D other)
+	{
 		if (other.tag == "Water") {
 			isInWater = false;
-			rb.drag = 1;
+			rb.drag = originalDrag;
 		}
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		string jumpButton = tag == "Player" ? "Jump" : "Jump_2";
-		if (Input.GetButton (jumpButton) && Time.time > nextFire) {
+		if (Input.GetButton (fire1Button) && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			GameObject clone = Instantiate (projectile, gunPoint.position, gunPoint.rotation) as GameObject;
 			clone.GetComponent<Rigidbody2D> ().velocity = rb.velocity;
 		}
-		bool thrustersOn = Input.GetButton ("Fire2");
+		bool thrustersOn = Input.GetAxis (yAxis) > 0f;
 		if (thrustersOn != thruster.isPlaying) {
 			if (thrustersOn)
 				thruster.Play ();
@@ -57,27 +75,24 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		float turn = 0.0f;
-		float thrust = 0.0f;
-		if (tag == "Player") {
-			turn = Input.GetAxis ("Horizontal");
-			thrust = Input.GetButton ("Fire2") ? 1.0f : 0.0f;
-		} else {
-			turn = Input.GetAxis ("Horizontal_2");
-			thrust = Input.GetButton ("Fire2_2") ? 1.0f : 0.0f;
-		}
+		float turn = Input.GetAxis (xAxis);
 
-		float positionMagnitude = rb.position.magnitude;
 		float floatingAndGravityForceMagnitude = (isInWater ? -1.2f : 1f) * gravityForceMagnitude;
-		float thursterForceMagnitude = thrust * Mathf.Clamp ((120f - positionMagnitude) / 20f, 0f, 1f) * maxThrustPower;
-		if (isInWater) {
-			thursterForceMagnitude = Mathf.Min (thursterForceMagnitude, 0.8f * floatingAndGravityForceMagnitude);
+		float thursterForceMagnitude = 0f;
+		if (Vector2.Dot (rb.velocity, transform.up) < maxSpeed) {
+			float thrust = Mathf.Max (Input.GetAxis (yAxis), 0f);
+			float athmosphereCoefficient = Mathf.Clamp ((120f - rb.position.magnitude) / 20f, 0f, 1f);
+			thursterForceMagnitude = thrust * athmosphereCoefficient * maxThrustPower;
+			if (isInWater) {
+				thursterForceMagnitude = Mathf.Min (thursterForceMagnitude, 0.8f * floatingAndGravityForceMagnitude);
+			}
 		}
 			
 		Vector2 gravity = rb.position.normalized * floatingAndGravityForceMagnitude;
 		Vector2 thrusters = transform.up * thursterForceMagnitude;
 
 		rb.AddForce (thrusters + gravity);
+		// TODO use rb.AddTorque and angularDrag to turn the ship
 		rb.angularVelocity = -turn * 90f;
 	}
 }
